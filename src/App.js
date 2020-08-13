9fcf7a79f5f1986ad5013ec6f493804e5680d5a4
VetalSh <jedvemya86@gmail.com>
2020-08-13 22:14:59 +0300

    Day2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Total } from './components/total/Total';
+import History from './components/history/History';
+import Operation from './components/operation/Operation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+          <h1>Кошелек</h1>
+          <h2>Калькулятор расходов</h2>
       </header>
-    </div>
+
+      <main>
+          <div className="container">
+              <Total/>
+              <History/>
+              <Operation/>
+          </div>
+      </main>
+    </>
+
   );
 }
 
