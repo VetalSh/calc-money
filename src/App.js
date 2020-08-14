76f2471f547c5addd4d5a4c2fb6cbd8a03afa7a2
VetalSh <jedvemya86@gmail.com>
2020-08-14 22:51:16 +0300

    Lesson2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Total } from './components/total/Total';
 import History from './components/history/History';
 import Operation from './components/operation/Operation';
 
-function App() {
-  return (
-    <>
-      <header>
+class App extends Component {
+
+  // Сохраняем данные
+  state = {
+    transactions: [],
+    description: '',
+    amount: '',
+  }
+  
+
+  addTransaction = add => {
+
+    const transactions = [...this.state.transactions,
+      {
+        id: `cmr${(+new Date).toString(16)}`,
+        description: this.state.description,
+        amount: this.state.amount,
+        add
+      }
+    ];
+
+    this.setState({
+      transactions,
+      description: '',
+      amount: '',
+    });
+  }
+
+  addAmount = e => {
+    this.setState({amount: e.target.value});
+  }
+
+  addDescription = e => {
+    this.setState({description: e.target.value});
+  }
+
+  render() {    
+    // console.log(this.state);
+    return (
+      <>
+        <header>
           <h1>Кошелек</h1>
           <h2>Калькулятор расходов</h2>
-      </header>
-
-      <main>
+        </header>
+  
+        <main>
           <div className="container">
               <Total/>
-              <History/>
-              <Operation/>
+              <History transactions={this.state.transactions}/>
+              <Operation 
+                addTransaction={this.addTransaction}
+                addAmount={this.addAmount}
+                addDescription={this.addDescription}
+                description={this.state.description}
+                amount={this.state.amount}
+              />
           </div>
-      </main>
-    </>
-
-  );
+        </main>
+      </>
+    );
+  }
 }
 
 export default App;
